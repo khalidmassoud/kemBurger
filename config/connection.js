7517ca44940b98fc5dfa1672fa6349de2014e9c3
khalidmassoud <khalid.massoud@hotmail.com>
2020-01-26 19:20:31 -0500

    added Heroku env

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,10 @@ var mysql = require("mysql");
 
 var connection;
 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
 connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -10,6 +14,7 @@ connection = mysql.createConnection({
   password: "password",
   database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
